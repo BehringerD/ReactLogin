aec70ff87fa7d31347703d12afafa258c84dd7e5
BehringerD <behringer.dani@gmail.com>
2026-06-17 20:39:54 +0700

    Fixed minor border issue on small and extra small screens.

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,7 @@ const App = () => {
         </div>
       </div>
         {/* Right Column with graphics */}
-        <div className="grid items-center md:rounded-b-lg lg:rounded-r-lg lg:rounded-l-none bg-[#161950] p-4 text-white">
+        <div className="grid items-center xs:rounded-b-lg sm:rounded-b-lg md:rounded-b-lg lg:rounded-r-lg lg:rounded-l-none bg-[#161950] p-4 text-white">
             <div className="text-center flex flex-col items-center justify-center">
               <img src={logo} alt="" style={{'--image-url': `url(${grid})`}} className="center tempus_logo max-w-80 bg-center bg-no-repeat bg-[image:var(--image-url)]"/>
               <p className="tempus_text opacity-50 text-center">Közös küldetés</p>
