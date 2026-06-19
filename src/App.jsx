f1eaed1b87f60577429d9a40b619c5e41568c226
BehringerD <behringer.dani@gmail.com>
2026-06-19 14:54:48 +0700

    modified main container for better centering and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import Login from './components/Login'
 const App = () => {
   return (
     // Main element
-    <>
+    <div className="flex min-h-screen items-center justify-center">
       {/* Initial 2 column grid setup. */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 m-4 border-2 border-slate-200 rounded-lg shadow-lg">
+      <div className="grid grid-cols-1 max-lg:w-full lg:grid-cols-2 m-4 border-2 border-slate-200 rounded-lg shadow-lg">
         
         {/* Left column with form. */}
         <Login />
@@ -18,7 +18,7 @@ const App = () => {
         <Images />
 
       </div>
-    </>
+    </div>
   )
 }
 
